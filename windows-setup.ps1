fd55d0ee6d55c3694e77c59bcb97a7b6aac31788
Thomas Gohard <thomasgohard@users.noreply.github.com>
2022-10-27 20:54:22 -0400

    WIP

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -16,17 +16,27 @@ foreach ($app in $apps) {
 	if ($null -ne $packageDetails) {
 		Write-Host "Already installed."
 	} else {
+		$cursorPosition = $host.UI.RawUI.CursorPosition
+
 		Write-Host "Not installed. " -NoNewline
+		$lineLength = $host.UI.RawUI.CursorPosition.X - $cursorPosition.X
+		$host.UI.RawUI.CursorPosition = $cursorPosition
 
 		$packageAvailable = Find-WinGetPackage -ID $app.id -Exact
 		if ($null -eq $packageAvailable) {
 			Write-Host "Application not available from $appSource."
 		} else {
 			Write-Host "Application found in $appSource. " -NoNewline
+			$host.UI.RawUI.CursorPosition = $cursorPosition
 
 			Install-WinGetPackage -ID $app.id -Exact
+			$host.UI.RawUI.CursorPosition = $cursorPosition
 
-			Write-Host "$($app.id) installed."
+			Write-Host "$($app.id) installed." -NoNewline
+			for ($i = 0; $i -lt 9; $i++) {
+				Write-Host " " -NoNewline
+			}
+			Write-Host ""
 		}
 	}
 }
